ba3197a03355fa4b8fcaec5fe6d7697c07a61618
alexandrejuk <ale_santos.soares@hotmail.com>
2017-12-09 00:51:51 -0200

    chore: created controllers getById and put and new routes

--- controllers/group.js
+++ controllers/group.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const Group = require('../models/group');
 
-
-const getGroup = (req, res, next) => {
-    Group.find({})
+const getGroups = (req, res, next) => {
+  Group.find({})
+    .then(group => res.json(group))
+    .catch(err => next(err));
+};
+const getGroupById = (req, res, next) => {
+  const id = req.params.id;
+  Group.find({ _id: id })
     .then(group => res.json(group))
     .catch(err => next(err));
 };
@@ -15,6 +20,14 @@ const postGroup = (req, res, next) => {
     .then(group => res.json(group))
     .catch(err => next(err));
 };
+const putGroup = (req, res, next) => {
+  const id = req.params.id;
+  const group = req.body;
+  Group.findByIdAndUpdate(id, group, {
+    runValidators: true
+  })
+    .then(group => res.json(group))
+    .catch(err => next(err));
+};
 
-
-module.exports = { getGroup, postGroup };
+module.exports = { getGroups, getGroupById, postGroup, putGroup };
--- routes/group.js
+++ routes/group.js
@@ -2,7 +2,10 @@ const express = require('express');
 const route = express.Router();
 const groupCtrl = require('../controllers/group');
 
-route.get('/group', groupCtrl.getGroup);
+route.get('/group', groupCtrl.getGroups);
+route.get('/group/:id', groupCtrl.getGroupById);
 route.post('/group', groupCtrl.postGroup);
+route.put('/group/:id', groupCtrl.putGroup);
+
 
 module.exports = route;
